3cd2254f4c109174bbaa5b16ef1455a15c2948b1
mpfarias <fariasmauricio11@outlook.com>
2024-05-11 20:44:45 -0300

    Add login_inc.php

---
+++ inc/login_inc.php
@@ -0,0 +1,20 @@
+<?php
+
+require_once "validateEmail.php";
+require_once "validateInput.php";
+require_once "errorMessage.php";
+
+function processLogin(): void
+{
+    try {
+        if ($_SERVER["REQUEST_METHOD"] === "POST") {
+
+            $username = validateInput("username", $_POST["username"]);
+            $password = validateInput("password", $_POST["password"]);
+        }
+    } catch (Exception $e) {
+        echo errorMessage($e->getMessage());
+    }
+}
+
+processLogin();
