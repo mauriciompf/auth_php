6e86b4ce19e4486b7c71807d851e3084fa183892
mpfarias <fariasmauricio11@outlook.com>
2024-05-13 12:28:18 -0300

    Update validateInput.php

--- inc/validateInput.php
+++ inc/validateInput.php
@@ -7,6 +7,37 @@ function validateInput(string $field, string $input)
         throw new Exception(ucfirst($field) . " cannot be empty. <br>");
     }
 
+    inputLenLimit($input, $field);
+
     // Sanitize input
     return htmlentities($input, ENT_QUOTES, "UTF-8");
+}
+
+
+function inputLenLimit($input, $field)
+{
+    $usernameLenLimit = 50;
+    $emailLenLimit = 255;
+    $passwordLenLimit = 55;
+
+    $limit = 0;
+
+    switch ($input) {
+        case $_POST["username"]:
+            $limit = $usernameLenLimit;
+            break;
+        case $_POST["email"]:
+            $limit = $emailLenLimit;
+            break;
+        case $_POST["password"]:
+            $limit = $passwordLenLimit;
+            break;
+        default:
+            throw new Exception("invalid input. <br>");
+            break;
+    }
+
+    if (strlen($input) >= $limit) {
+        throw new Exception(ucfirst($field) . " cannot be more than $limit caracteres.<br>");
+    }
 }
