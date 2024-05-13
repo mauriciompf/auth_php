d80640d7559a61a523b3a8bbe8d7a8454e0c8560
mpfarias <fariasmauricio11@outlook.com>
2024-05-13 12:28:26 -0300

    Add prepareData.php

---
+++ inc/prepareData.php
@@ -0,0 +1,25 @@
+<?php
+
+require_once "database.php";
+require_once "errorMessage.php";
+include "createTable.php";
+
+function prepareData($conn, $username, $email, $password)
+{
+    try {
+        global $tableName;
+        $stmt = $conn->prepare("INSERT INTO $tableName (username, email, password)
+        VALUES (:username, :email, :password)");
+
+        $stmt->bindParam(":username", $username);
+        $stmt->bindParam(":email", $email);
+        $stmt->bindParam(":password", $password);
+        $stmt->execute();
+
+        echo "Inserted data successfully. <br>";
+
+        $conn = null;
+    } catch (PDOException $e) {
+        echo errorMessage($e->getMessage());
+    }
+}
