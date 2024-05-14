67b9555a819b0b150225e891380c94d59c1b99a2
mpfarias <fariasmauricio11@outlook.com>
2024-05-14 15:27:42 -0300

    Update validateInput.php

--- inc/validateInput.php
+++ inc/validateInput.php
@@ -13,14 +13,29 @@ function validateInput(string $field, string $input)
     inputLenLimit($input, $field);
 
     if ($input === $_POST["username"]) {
-
-        $exp = "/^(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$/";
+        $exp = "/^(?![_.])(?!.*[_.]{2})[a-zA-ZÀ-ÿ0-9._]+(?<![_.])$/";
         if (!preg_match($exp, $input)) {
             throw new Exception("$field input invalid. <br>");
         }
 
         if (strlen($input) < 4) {
             throw new Exception($field . " should be at least 4 caracteres. <br>");
+        }
+    }
+
+    if ($input === $_POST["password"]) {
+        // - Contain at least one special character (!@#$%^&*()-_=+{};:,<.>)
+        // - Contain at least one uppercase letter
+        // - Contain at least one digit
+        // - Contain only alphanumeric characters and the specified special characters
+        $exp = "/^(?=.*[a-zA-Z0-9])(?=.*[!@#$%^&*()-_=+{};:,<.>])(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9!@#$%^&*()-_=+{};:,<.>]+$/";
+
+        if (!preg_match($exp, $input)) {
+            throw new Exception($field . " input invalid. <br>");
+        }
+
+        if (strlen($input) < 6) {
+            throw new Exception($field . " should be at least 6 caracteres. <br>");
         }
     }
 
