6e3274c8f54ff1239a0a4eb4e9ea2d6c31239ae0
mpfarias <fariasmauricio11@outlook.com>
2024-05-13 21:32:31 -0300

    Update validateInput.php

--- inc/validateInput.php
+++ inc/validateInput.php
@@ -2,20 +2,37 @@
 
 function validateInput(string $field, string $input)
 {
+
+    $field = ucfirst($field);
+
     // Check if is empty
     if (empty($input)) {
-        throw new Exception(ucfirst($field) . " cannot be empty. <br>");
+        throw new Exception($field . " cannot be empty. <br>");
     }
 
     inputLenLimit($input, $field);
 
+    if ($input === $_POST["username"]) {
+
+        $exp = "/^(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$/";
+        if (!preg_match($exp, $input)) {
+            throw new Exception("$field input invalid. <br>");
+        }
+
+        if (strlen($input) < 4) {
+            throw new Exception($field . " should be at least 4 caracteres. <br>");
+        }
+    }
+
     // Sanitize input
-    return htmlentities($input, ENT_QUOTES, "UTF-8");
+    return trim(htmlentities($input, ENT_QUOTES, "UTF-8"));
 }
 
 
 function inputLenLimit($input, $field)
 {
+
+    $field = ucfirst($field);
     $usernameLenLimit = 50;
     $emailLenLimit = 255;
     $passwordLenLimit = 55;
