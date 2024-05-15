07ff5980eb0fd2e8d9d5c4d66ea68880c71a2792
mpfarias <fariasmauricio11@outlook.com>
2024-05-15 15:38:17 -0300

    Update authentication.php

---
+++ inc/authentication.php
@@ -0,0 +1,40 @@
+<?php
+
+// require_once "validateInput.php";
+require_once "errorMessage.php";
+require_once "database.php";
+
+function processLogin(): void
+{
+    session_start();
+    try {
+        if ($_SERVER["REQUEST_METHOD"] === "POST") {
+            // $username = validateInput("username", $_POST["username"]);
+            // $password = validateInput("password", $_POST["password"]);
+
+            $username = $_POST["username"];
+            $password = $_POST["password"];
+
+            $conn = createDB();
+            $sql = "SELECT id, username, password
+            FROM registration
+            WHERE username = ?";
+            $stmt = $conn->prepare($sql);
+            $stmt->execute([$username]);
+            $user = $stmt->fetch();
+
+            if (!$user && !password_verify($password, $user["password"])) {
+                throw new Exception("Invalid username or password");
+            }
+
+            $_SESSION["id"] = $user["id"];
+            $_SESSION["username"] = $user["username"];
+            header("Location: ./mainPage.php");
+            exit;
+        }
+    } catch (Exception $e) {
+        echo errorMessage($e->getMessage() .  ". <br>");
+    }
+}
+
+processLogin();
